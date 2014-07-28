7fd26bcedc61880035ce478eed03fd02f916e368
Damien Duboeuf <d.duboeuf@glowbl.com>
2014-07-28 17:01:49 +0200

    Ajout du World Generatore

--- src/minecraft/mods/gollum/core/ModGollumCoreLib.java
+++ src/minecraft/mods/gollum/core/ModGollumCoreLib.java
@@ -1,20 +1,20 @@
 
 package mods.gollum.core;
 
+import mods.gollum.core.blocks.BlockSpawner;
 import mods.gollum.core.config.ConfigLoader;
 import mods.gollum.core.config.ConfigProp;
 import mods.gollum.core.log.Logger;
+import mods.gollum.core.tileentities.TileEntityBlockSpawner;
 import mods.gollum.core.version.VersionChecker;
 import net.minecraft.block.Block;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
-import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
-import cpw.mods.fml.common.registry.LanguageRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
 
 @Mod(modid = "GollumCoreLib", name = "Gollum Core Lib", version = "1.0.0", acceptedMinecraftVersions = "1.6.4")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
@@ -26,7 +26,12 @@ public class ModGollumCoreLib {
 	
 	@ConfigProp (info = "Display version checker message")
 	public static boolean versionChecker = true;
-
+	
+	@ConfigProp(group = "Blocks Ids")
+	public static int blockSpawnerID = 1243;
+	
+	public static Block blockSpawner;
+	
 	/**
 	 * Gestion des logs
 	 */
@@ -51,13 +56,36 @@ public class ModGollumCoreLib {
 	/** 2 **/
 	@EventHandler
 	public void load(FMLInitializationEvent event) {
-		
+
+		// Initialisation des blocks
+		this.initBlocks ();
+
+		// Initialisation les TileEntities
+		this.initTileEntities ();
 	}
 
 	/** 3 **/
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event) {
 
+	}
+
+	/**
+	 * // Nom des TileEntities
+	 */
+	private void initTileEntities () {
+		GameRegistry.registerTileEntity(TileEntityBlockSpawner.class, "GollumCoreLib:BlockSpawner");
+	}
+	
+	/**
+	 * Initialisation des blocks
+	 */
+	public void initBlocks () {
+
+		// Création des blocks
+		this.blockSpawner = (new BlockSpawner(this.blockSpawnerID)).setUnlocalizedName("GCLBlockSpawner");
+		GameRegistry.registerBlock(this.blockSpawner, "GCLblockSpawner");
+		
 	}
 	
 }
