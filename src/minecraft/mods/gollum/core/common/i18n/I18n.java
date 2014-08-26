8ca5d0c6de759cc277799f6a1536c40f43ef6eee
Damien Duboeuf <smeagolworms4@gmail.com>
2014-08-26 18:06:40 +0200

    Add transforme i18n

--- src/minecraft/mods/gollum/core/common/i18n/I18n.java
+++ src/minecraft/mods/gollum/core/common/i18n/I18n.java
@@ -11,8 +11,34 @@ public class I18n {
 	public I18n() {
 		modId = ModContext.instance().getCurrent().getModId().toLowerCase();
 	}
-	
+
 	public String trans (String key, Object ... args) {
 		return StatCollector.translateToLocalFormatted(this.modId+"."+key, args);
+	}
+
+	public int transInt (String key, Object ... args) {
+		String s = StatCollector.translateToLocalFormatted(this.modId+"."+key, args);
+		int rtn = 0; try { rtn = Integer.parseInt(s); } catch (Exception e) {}
+		return rtn;
+	}
+	public long transLong (String key, Object ... args) {
+		String s = StatCollector.translateToLocalFormatted(this.modId+"."+key, args);
+		long rtn = 0; try { rtn = Long.parseLong(s); } catch (Exception e) {}
+		return rtn;
+	}
+	public boolean transBoolean (String key, Object ... args) {
+		String s = StatCollector.translateToLocalFormatted(this.modId+"."+key, args);
+		boolean rtn = false; try { rtn = Boolean.parseBoolean(s); } catch (Exception e) {}
+		return rtn;
+	}
+	public float transFloat (String key, Object ... args) {
+		String s = StatCollector.translateToLocalFormatted(this.modId+"."+key, args);
+		float rtn = 0; try { rtn = Float.parseFloat(s); } catch (Exception e) {}
+		return rtn;
+	}
+	public double transDouble (String key, Object ... args) {
+		String s = StatCollector.translateToLocalFormatted(this.modId+"."+key, args);
+		double rtn = 0; try { rtn = Double.parseDouble(s); } catch (Exception e) {}
+		return rtn;
 	}
 }
