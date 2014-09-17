aab26ecd780bd62f73eec881777d0b7aeed662d3
Damien Duboeuf <smeagolworms4@gmail.com>
2014-09-17 12:30:29 +0200

    Num version

--- src/main/java/mods/gollum/core/ModGollumCoreLib.java
+++ src/main/java/mods/gollum/core/ModGollumCoreLib.java
@@ -35,7 +35,7 @@ public class ModGollumCoreLib extends GollumMod {
 	public final static String MODID = "GollumCoreLib";
 	public final static String MODNAME = "Gollum Core Lib";
 	public final static String VERSION = "2.0.0";
-	public final static String MINECRAFT_VERSION = "1.6.4";
+	public final static String MINECRAFT_VERSION = "1.7.2";
 
 	@Instance(ModGollumCoreLib.MODID)
 	public static ModGollumCoreLib instance;
