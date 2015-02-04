16c93b98da8d53ea7890d0924cd1ed25d0d2705d
SmeagolWorms4 <smeagolworms4@gmail.com>
2015-02-04 11:55:54 +0100

    Name of variable

--- src/main/java/mods/gollum/core/tools/registry/RenderingRegistry.java
+++ src/main/java/mods/gollum/core/tools/registry/RenderingRegistry.java
@@ -4,9 +4,9 @@ import cpw.mods.fml.client.registry.ISimpleBlockRenderingHandler;
 
 public class RenderingRegistry {
 	
-	public static int registerBlockHandler(ISimpleBlockRenderingHandler handler) {
-		int id = cpw.mods.fml.client.registry.RenderingRegistry.getNextAvailableRenderId();;
-		cpw.mods.fml.client.registry.RenderingRegistry.registerBlockHandler(id, handler);
+	public static int registerBlockHandler(ISimpleBlockRenderingHandler renderer) {
+		int id = cpw.mods.fml.client.registry.RenderingRegistry.getNextAvailableRenderId();
+		cpw.mods.fml.client.registry.RenderingRegistry.registerBlockHandler(id, renderer);
 		return id;
 	}
 }
