1477f060cc0da388aa2a6921aa78e0882236257a
SmeagolWorms4 <smeagolworms4@gmail.com>
2015-05-10 05:56:29 +0200

    Loasing sound

--- src/main/java/com/gollum/core/client/ClientProxyGolumCoreLib.java
+++ src/main/java/com/gollum/core/client/ClientProxyGolumCoreLib.java
@@ -3,15 +3,11 @@ package com.gollum.core.client;
 import net.minecraftforge.common.MinecraftForge;
 
 import com.gollum.core.common.CommonProxyGolumCoreLib;
-import com.gollum.core.tools.registry.SoundRegistry;
 
 public class ClientProxyGolumCoreLib extends CommonProxyGolumCoreLib {
 
 	public void registerEvents () {
-		
 		super.registerEvents ();
-		
-		MinecraftForge.EVENT_BUS.register(new SoundRegistry());
 	}
 
 	public boolean isRemote() {
