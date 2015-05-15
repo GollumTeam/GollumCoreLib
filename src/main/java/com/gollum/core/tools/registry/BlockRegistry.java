c69ec88c21ba880d7fd9af415cf9fa61f6449ced
SmeagolWorms4 <smeagolworms4@gmail.com>
2015-05-15 02:31:50 +0200

    Override Block

--- src/main/java/com/gollum/core/tools/registry/BlockRegistry.java
+++ src/main/java/com/gollum/core/tools/registry/BlockRegistry.java
@@ -1,8 +1,17 @@
 package com.gollum.core.tools.registry;
 
+import static com.gollum.core.ModGollumCoreLib.log;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+
 import com.gollum.core.tools.helper.IBlockHelper;
+import com.gollum.core.utils.reflection.Reflection;
+import com.gollum.morepistons.common.item.ItemMorePistonsVanillaProxy;
 
 
 public class BlockRegistry {
@@ -28,76 +37,61 @@ public class BlockRegistry {
 		this.blocks.clear();
 	}
 	
-	// TODO a revoir
-//	public void overrideBlocksClassField (Block vanillaBlock, Block block) {
-//		
-//		try {
-//			
-//			Field field = null;
-//			
-//			for (Field f : Blocks.class.getFields()) {
-//				f.setAccessible(true);
-//				if (Modifier.isStatic(f.getModifiers()) && f.get(null) == vanillaBlock) {
-//					field = f;
-//				}
-//			}
-//			if (field == null) {
-//				throw new Exception("This block \""+vanillaBlock.getClass().getName()+"\" not found in fields "+Blocks.class.getName());
-//			}
-//			
-//			this.overrideBlocksClassField(field.getName(), block);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	public void overrideBlocksClassField (String fieldName, Block block) {
-//		try {
-//			Reflection.setFinalStatic(Blocks.class.getDeclaredField(fieldName), block);
-//			log.message("Override Blocks field fieldName=\""+fieldName+"\" by "+block.getClass().getSimpleName());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	
-//	public void overrideRegistered (String registerName, Block block) {
-//		
-//		try {
-//			
-//			Block vanillaBlock = RegisteredObjects.instance().getBlock(registerName);
-//			
-//			if (vanillaBlock != null) {
-//				
-//				log.message("Override registery block \""+registerName+"\" by "+block.getClass().getSimpleName());
-//				
-//				FMLControlledNamespacedRegistry<Block> blockRegistry = GameData.getBlockRegistry();
-//				Field f1 = RegistrySimple.class.getDeclaredField("registryObjects");
-//				f1.setAccessible(true);
-//				Map registryObjects = (Map)f1.get(blockRegistry);
-//				registryObjects.put(registerName, block);
-//				log.debug (" 1 - Replace \""+registerName+"\" registery : registryObjects");
-//				
-//
-//				Field f2 = RegistryNamespaced.class.getDeclaredField("underlyingIntegerMap");
-//				f2.setAccessible(true);
-//				ObjectIntIdentityMap underlyingIntegerMap = (ObjectIntIdentityMap)f2.get(blockRegistry);
-//				
-//				int id = underlyingIntegerMap.func_148747_b(vanillaBlock);
-//				if (id == -1) {
-//					throw new Exception(" 2 - Replace \""+registerName+"\" registery is KO because id not found");
-//				}
-//				underlyingIntegerMap.func_148746_a(block, id);
-//				log.debug (" 2 - Replace \""+registerName+"\" registery : underlyingIntegerMap id="+id);
-//				
-//			} else {
-//				log.severe("The original block \""+registerName+"\" not found for replace registery.");
-//			}
-//			
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//		
-//	}
+	public void overrideBlocksClassField (Block vanillaBlock, Block block) {
+		
+		try {
+			
+			Field field = null;
+			
+			for (Field f : Block.class.getFields()) {
+				f.setAccessible(true);
+				if (Modifier.isStatic(f.getModifiers()) && f.get(null) == vanillaBlock) {
+					field = f;
+				}
+			}
+			if (field == null) {
+				throw new Exception("This block \""+vanillaBlock.getClass().getName()+"\" not found in fields "+Block.class.getName());
+			}
+			
+			this.overrideBlocksClassField(field.getName(), block);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void overrideBlocksClassField (String fieldName, Block block) {
+		try {
+			Reflection.setFinalStatic(Block.class.getDeclaredField(fieldName), block);
+			log.message("Override Blocks field fieldName=\""+fieldName+"\" by "+block.getClass().getSimpleName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void overrideBlocksId (Block vanillaBlock, Block block) {
+		
+		int newId = vanillaBlock.blockID;
+		int oldId = block.blockID;
+		
+		try {
+			
+			Field f = null;
+			try {
+				f = Block.class.getDeclaredField("=======");
+			} catch (Exception e) {
+				log.message("Unofuscate property RegistrySimple : ======= => blockId");
+				f = Block.class.getDeclaredField("blockID");
+			}
+			Reflection.setFinalField(f, block, newId);
+			log.message("Override block field fieldName=\"blockID by "+newId);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Block.blocksList[newId] = block;
+		Block.blocksList[oldId] = null;
+		
+	}
 	
 }
--- src/main/java/com/gollum/core/tools/registry/ItemRegistry.java
+++ src/main/java/com/gollum/core/tools/registry/ItemRegistry.java
@@ -1,8 +1,16 @@
 package com.gollum.core.tools.registry;
 
+import static com.gollum.core.ModGollumCoreLib.log;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.ArrayList;
 
+import net.minecraft.item.Item;
+
 import com.gollum.core.tools.helper.IItemHelper;
+import com.gollum.core.utils.reflection.Reflection;
+import com.gollum.morepistons.common.item.ItemMorePistonsVanillaProxy;
 
 
 public class ItemRegistry {
@@ -28,37 +36,62 @@ public class ItemRegistry {
 		this.items.clear();
 	}
 	
-	// TODO a revoir
-//	public void overrideItemsClassField (Item vanillaItem, Item item) {
-//		
-//		try {
-//			
-//			Field field = null;
-//			
-//			for (Field f : Items.class.getFields()) {
-//				f.setAccessible(true);
-//				if (Modifier.isStatic(f.getModifiers()) && f.get(null) == vanillaItem) {
-//					field = f;
-//				}
-//			}
-//			if (field == null) {
-//				throw new Exception("This item \""+vanillaItem.getClass().getName()+"\" not found in fields "+Items.class.getName());
-//			}
-//			
-//			this.overrideItemsClassField(field.getName(), item);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
-//	
-//	public void overrideItemsClassField (String fieldName, Item item) {
-//		try {
-//			Reflection.setFinalStatic(Items.class.getDeclaredField(fieldName), item);
-//			log.message("Override Items field fieldName=\""+fieldName+"\" by "+item.getClass().getSimpleName());
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public void overrideItemsClassField (Item vanillaItem, Item item) {
+		
+		try {
+			
+			Field field = null;
+			
+			for (Field f : Item.class.getFields()) {
+				f.setAccessible(true);
+				if (Modifier.isStatic(f.getModifiers()) && f.get(null) == vanillaItem) {
+					field = f;
+				}
+			}
+			if (field == null) {
+				throw new Exception("This item \""+vanillaItem.getClass().getName()+"\" not found in fields "+Item.class.getName());
+			}
+			
+			this.overrideItemsClassField(field.getName(), item);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void overrideItemsClassField (String fieldName, Item item) {
+		try {
+			Reflection.setFinalStatic(Item.class.getDeclaredField(fieldName), item);
+			log.message("Override Items field fieldName=\""+fieldName+"\" by "+item.getClass().getSimpleName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void overrideItemId(Item vanillaItem, Item item) {
+		
+		int newId = vanillaItem.itemID;
+		int oldId = item.itemID;
+		
+		try {
+			
+			Field f = null;
+			try {
+				f = Item.class.getDeclaredField("=======");
+			} catch (Exception e) {
+				log.message("Unofuscate property RegistrySimple : ======= => blockId");
+				f = Item.class.getDeclaredField("itemID");
+			}
+			Reflection.setFinalField(f, item, newId);
+			log.message("Override Item field fieldName=\"itemID by "+newId);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Item.itemsList[newId] = item;
+		
+	}
+	
 //	
 //	
 //	public void overrideRegistered (String registerName, Item item) {
