eb0f5718cc2726ab055243f51f4c537d04874d6d
SmeagolWorms4 <smeagolworms4@gmail.com>
2015-06-21 13:38:38 +0200

    Merge feature form 1.7.10

---
+++ src/main/java/com/gollum/core/common/resources/ResourcePackFileNotFoundException.java
@@ -0,0 +1,14 @@
+package com.gollum.core.common.resources;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ResourcePackFileNotFoundException extends FileNotFoundException {
+	private static final String __OBFID = "CL_00001086";
+	
+	public ResourcePackFileNotFoundException(File p_i1294_1_, String p_i1294_2_) {
+	}
+}
