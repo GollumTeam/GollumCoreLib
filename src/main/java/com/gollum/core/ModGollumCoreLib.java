7e9860c11147244756df374d06e3e90e6ed63154
SmeagolWorms4 <smeagolworms4@gmail.com>
2015-06-27 23:19:51 +0200

    Fix for 1.7.2

--- src/main/java/com/gollum/core/ModGollumCoreLib.java
+++ src/main/java/com/gollum/core/ModGollumCoreLib.java
@@ -41,7 +41,7 @@ public class ModGollumCoreLib extends GollumMod {
 	public final static String MODID = "GollumCoreLib";
 	public final static String MODNAME = "Gollum Core Lib";
 	public final static String VERSION = "2.0.0DEV";
-	public final static String MINECRAFT_VERSION = "1.7.10";
+	public final static String MINECRAFT_VERSION = "1.7.2";
 
 	@Instance(ModGollumCoreLib.MODID)
 	public static ModGollumCoreLib instance;
