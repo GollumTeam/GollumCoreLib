6d8a9f37f21faec5665da0d8e72cadfb8b0c7216
SmeagolWorms4 <smeagolworms4@gmail.com>
2015-11-27 23:20:42 +0100

    Update version 2.1.0

--- src/main/java/com/gollum/core/ModGollumCoreLib.java
+++ src/main/java/com/gollum/core/ModGollumCoreLib.java
@@ -40,7 +40,7 @@ public class ModGollumCoreLib extends GollumMod {
 
 	public final static String MODID = "GollumCoreLib";
 	public final static String MODNAME = "Gollum Core Lib";
-	public final static String VERSION = "2.0.1DEV";
+	public final static String VERSION = "2.1.0DEV";
 	public final static String MINECRAFT_VERSION = "1.7.10";
 
 	@Instance(ModGollumCoreLib.MODID)
