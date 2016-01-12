8379b367f345b6cefbd772cf257677ac5a5a0838
SmeagolWorms4 <smeagolworms4@gmail.com>
2016-01-13 00:20:39 +0100

    Fix Item infos

--- src/main/java/com/gollum/core/common/blocks/IBlockDisplayInfos.java
+++ src/main/java/com/gollum/core/common/blocks/IBlockDisplayInfos.java
@@ -1,9 +1,10 @@
 package com.gollum.core.common.blocks;
 
+import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 
 public interface IBlockDisplayInfos {
 	
-	public String displayDebugInfos(World world, int x, int y, int z);
+	public String displayDebugInfos(World world, BlockPos pos);
 
 }
