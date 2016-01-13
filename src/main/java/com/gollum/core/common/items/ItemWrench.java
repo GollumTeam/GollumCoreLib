8451a16d3e7eae739baa3328eaf2f878d903fcd2
Damien Duboeuf <smeagolworms4@gmail.com>
2016-01-13 16:54:07 +0100

    Fiw Item Wrench

--- src/main/java/com/gollum/core/common/items/ItemWrench.java
+++ src/main/java/com/gollum/core/common/items/ItemWrench.java
@@ -18,39 +18,25 @@ import net.minecraft.world.World;
 
 public class ItemWrench extends HItem {
 	
-	public Set<Class<? extends Block>> shiftRotations = new HashSet<Class<? extends Block>>();
-	
 	public ItemWrench(String registerName) {
 		super(registerName);
 		
 		this.setFull3D();
 		this.setMaxStackSize(1);
-		this.shiftRotations.add(BlockLever.class);
-		this.shiftRotations.add(BlockButton.class);
-		this.shiftRotations.add(BlockChest.class);
 		this.setHarvestLevel("wrench", 0);
 	}
 
-	private boolean isShiftRotation(Class<? extends Block> cls) {
-		for (Class<? extends Block> shift : shiftRotations) {
-			if (shift.isAssignableFrom(cls)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-    /**
-     * This is called when the item is used, before the block is activated.
-     * @param stack The Item Stack
-     * @param player The Player that used the item
-     * @param world The Current World
-     * @param pos Target position
-     * @param side The side of the target hit
-     * @return Return true to prevent any further processing.
-     */
+	/**
+	 * This is called when the item is used, before the block is activated.
+	 * @param stack The Item Stack
+	 * @param player The Player that used the item
+	 * @param world The Current World
+	 * @param pos Target position
+	 * @param side The side of the target hit
+	 * @return Return true to prevent any further processing.
+	 */
 	@Override
-    public boolean onItemUseFirst(ItemStack itemStack, EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ) {
+	public boolean onItemUseFirst(ItemStack itemStack, EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ) {
 		
 		IBlockState state = world.getBlockState(pos);
 		if (state == null) {
@@ -62,14 +48,10 @@ public class ItemWrench extends HItem {
 		if (block == null) {
 			return false;
 		}
-
-		if (player.isSneaking() != isShiftRotation(block.getClass())) {
-			return false;
-		}
 		
 		if (block.rotateBlock(world, pos, side)) {
 			player.swingItem();
-			return !world.isRemote;
+			return true;
 		}
 		return false;
 	}
