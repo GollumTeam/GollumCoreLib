56b0831e87019547d7697bc9ab1c17ef61197a36
SmeagolWorms4 <smeagolworms4@gmail.com>
2016-01-15 01:14:54 +0100

    Fix Item Wrench

--- src/main/java/com/gollum/core/common/items/ItemWrench.java
+++ src/main/java/com/gollum/core/common/items/ItemWrench.java
@@ -16,37 +16,19 @@ import net.minecraftforge.common.util.ForgeDirection;
 
 public class ItemWrench extends HItem {
 	
-	public Set<Class<? extends Block>> shiftRotations = new HashSet<Class<? extends Block>>();
-	
 	public ItemWrench(String registerName) {
 		super(registerName);
 		
 		this.setFull3D();
 		this.setMaxStackSize(1);
-		this.shiftRotations.add(BlockLever.class);
-		this.shiftRotations.add(BlockButton.class);
-		this.shiftRotations.add(BlockChest.class);
 		this.setHarvestLevel("wrench", 0);
 	}
-
-	private boolean isShiftRotation(Class<? extends Block> cls) {
-		for (Class<? extends Block> shift : shiftRotations) {
-			if (shift.isAssignableFrom(cls)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
+	
 	@Override
 	public boolean onItemUseFirst(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ) {
 		Block block = world.getBlock(x, y, z);
 		
 		if (block == null) {
-			return false;
-		}
-
-		if (player.isSneaking() != isShiftRotation(block.getClass())) {
 			return false;
 		}
 		
