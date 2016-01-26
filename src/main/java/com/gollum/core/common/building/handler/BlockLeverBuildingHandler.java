6b3453218195ed9a6f6fd63a83bc2a32443df1d1
SmeagolWorms4 <smeagolworms4@gmail.com>
2016-01-26 03:03:54 +0100

    Change affecte metadata on builder

--- src/main/java/com/gollum/core/common/building/handler/BlockLeverBuildingHandler.java
+++ src/main/java/com/gollum/core/common/building/handler/BlockLeverBuildingHandler.java
@@ -3,19 +3,20 @@ package com.gollum.core.common.building.handler;
 import com.gollum.core.ModGollumCoreLib;
 import com.gollum.core.common.building.Building.Unity;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockLever;
 import net.minecraft.world.World;
 
 public class BlockLeverBuildingHandler extends BuildingBlockHandler {
 	
 	@Override
-	protected boolean mustApply (World world, int x, int y, int z, Block block) {
-		return block instanceof BlockLever;
+	protected boolean mustApply (World world, int x, int y, int z, Unity unity) {
+		return unity.block instanceof BlockLever;
 	}
 	
 	@Override
-	public void applyOrientation(World world, int x, int y, int z, Block block, int metadata, int orientation, int rotate) {
+	protected int applyMetadata(World world, int x, int y, int z, int metadata, Unity unity, int rotate) {
+		
+		int orientation = this.rotateOrientation(rotate, unity.orientation);
 		
 		if (orientation == Unity.ORIENTATION_UP)    { metadata = (metadata & 0x8) + 4; } else 
 		if (orientation == Unity.ORIENTATION_DOWN)  { metadata = (metadata & 0x8) + 3; } else 
@@ -28,11 +29,10 @@ public class BlockLeverBuildingHandler extends BuildingBlockHandler {
 		if (orientation == Unity.ORIENTATION_TOP_VERTICAL)   { metadata = (metadata & 0x8) + 7; } else 
 		if (orientation == Unity.ORIENTATION_TOP_HORIZONTAL) { metadata = (metadata & 0x8) + 0; } else 
 		{
-			ModGollumCoreLib.log.severe("Bad orientation : "+orientation+" name:"+block.getUnlocalizedName()+" pos:"+x+","+y+","+z);
+			ModGollumCoreLib.log.severe("Bad orientation : "+orientation+" name:"+unity.block.getUnlocalizedName()+" pos:"+x+","+y+","+z);
 		}
 		
-		world.setBlockMetadataWithNotify(x, y, z, metadata, 0);
-		return;
+		return metadata;
 		
 	}
 	
--- src/main/java/com/gollum/core/common/building/handler/BuildingBlockHandler.java
+++ src/main/java/com/gollum/core/common/building/handler/BuildingBlockHandler.java
@@ -1,7 +1,9 @@
 package com.gollum.core.common.building.handler;
 
-import java.util.HashMap;
 import java.util.Random;
+
+import com.gollum.core.common.building.Building;
+import com.gollum.core.common.building.Building.Unity;
 
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
@@ -10,52 +12,94 @@ public abstract class BuildingBlockHandler {
 	
 	/**
 	 * Affecte l'orientation
+	 * @param metadata2 
+	 * @return TODO
 	 */
 	
-	public final void setOrientation(World world, int x, int y, int z, Block block, int metadata, int orientation, int rotate) {
-		if (this.mustApply(world, x, y, z, block)) {
-			this.applyOrientation(world, x, y, z, block, metadata, orientation, rotate);
+	public final int getMetadata(World world, int x, int y, int z, int metadata, Unity unity, int rotate) {
+		if (this.mustApply(world, x, y, z, unity)) {
+			metadata = this.applyMetadata(world, x, y, z, metadata, unity, rotate);
 		}
+		return metadata;
 	}
 
-	/**
-	 * Insert les extras informations du block
-	 * @param rotate	
-	 */
 	public final void setExtra(
 		Block block,
 		World world,
 		Random random, 
 		int x, int y, int z, 
-		HashMap<String, String> extra,
+		Unity unity,
 		int initX, int initY, int initZ, 
 		int rotate,
 		int dx, int dz,
 		int maxX, int maxZ
 	) {
-		if (this.mustApply(world, x, y, z, block)) {
-			this.applyExtra(block, world, random, x, y, z, extra, initX, initY, initZ, rotate, dx, dz, maxX, maxZ);
+		if (this.mustApply(world, x, y, z, unity)) {
+			this.applyExtra(block, world, random, x, y, z, unity, initX, initY, initZ, rotate, dx, dz, maxX, maxZ);
 		}
 	}
 	
-	protected boolean mustApply (World world, int x, int y, int z, Block block) {
+	protected boolean mustApply (World world, int x, int y, int z, Unity unity) {
 		return false;
 	}
 
-	protected void applyOrientation(World world, int x, int y, int z, Block block, int metadata, int orientation, int rotate) {
+	protected int applyMetadata(World world, int x, int y, int z, int metadata, Unity unity, int rotate) {
+		return metadata;
 	}
 	
 	protected void applyExtra(
 		Block block,
 		World world,
 		Random random, 
-		int x, int y, int z, 
-		HashMap<String, String> extra,
+		int x, int y, int z,
+		Unity unity,
 		int initX, int initY, int initZ, 
 		int rotate,
 		int dx, int dz,
 		int maxX, int maxZ
 	) {
+	}
+	
+	/**
+	 * Retourne l'orientation retourner en fonction de la rotation
+	 * @param rotate
+	 * @param orientation
+	 * @return
+	 */
+	protected int rotateOrientation(int rotate, int orientation) {
+		if (rotate == Building.ROTATED_90) {
+			
+			switch (orientation) { 
+				case Unity.ORIENTATION_UP:
+					return Unity.ORIENTATION_RIGTH;
+				case Unity.ORIENTATION_RIGTH:
+					return Unity.ORIENTATION_DOWN;
+				case Unity.ORIENTATION_DOWN:
+					return Unity.ORIENTATION_LEFT;
+				case Unity.ORIENTATION_LEFT:
+					return Unity.ORIENTATION_UP;
+					
+				case Unity.ORIENTATION_TOP_HORIZONTAL:
+					return Unity.ORIENTATION_TOP_VERTICAL;
+				case Unity.ORIENTATION_TOP_VERTICAL:
+					return Unity.ORIENTATION_TOP_HORIZONTAL;
+					
+				case Unity.ORIENTATION_BOTTOM_HORIZONTAL:
+					return Unity.ORIENTATION_BOTTOM_VERTICAL;
+				case Unity.ORIENTATION_BOTTOM_VERTICAL:
+					return Unity.ORIENTATION_BOTTOM_HORIZONTAL;
+					
+				default:
+					return Unity.ORIENTATION_NONE;
+			}
+		}
+		if (rotate == Building.ROTATED_180) {
+			return this.rotateOrientation(Building.ROTATED_90, this.rotateOrientation(Building.ROTATED_90, orientation));
+		}
+		if (rotate == Building.ROTATED_270) {
+			return this.rotateOrientation(Building.ROTATED_180, this.rotateOrientation(Building.ROTATED_90, orientation));
+		}
+		return orientation;
 	}
 	
 }
